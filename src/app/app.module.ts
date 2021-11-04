9c5333a47bf12763f95dd4d0be11add435cad0e2
codelaghien <codelaghien.github.io>
2021-11-04 09:11:42 -0700

    Parent - Child - Cha Con gọi nhau

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChaComponent } from './cha/cha.component';
+import { ConComponent } from './con/con.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PageNotFoundComponent, ChaComponent, ConComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
